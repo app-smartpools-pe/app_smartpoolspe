5c0fe674478cedb464744d161f84cfa0676cae0b
smartpoolspe <proof@smartpools.pe>
2025-06-11 11:51:50 -0500

    feat: update data

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 
 import "../css/globalappstyles.css";
 
 ReactDOM.createRoot(document.getElementById("page")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
